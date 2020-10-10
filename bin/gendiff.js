b88852a8b720a9f41ad771906a0ac161fbf69b7e
feycot <feycot@gmail.com>
2020-10-10 16:53:34 +0500

    add arguments and options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 import commander from '../node_modules/commander/index.js';
 
-commander.program.description('Compares two configuration files and shows a difference.');
-commander.program.version('1.0.0');
+commander.program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 commander.program.parse(process.argv);
