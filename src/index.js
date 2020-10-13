c3d7276149cf139731d09dcea28383eaf38dab60
feycot <feycot@gmail.com>
2020-10-13 08:52:58 +0500

    add compare plain obj

--- src/index.js
+++ src/index.js
@@ -12,13 +12,33 @@ const getFileData = (filePath) => {
 const jsonParser = (data) => JSON.parse(data);
 
 const buildDiff = (dataset1, dataset2) => {
-  const keys = _.union(_.keys(dataset1), _.keys(dataset2));
+  const keys = _.union(_.keys(dataset1), _.keys(dataset2)).sort();
 
   const diff = keys.map((key) => {
     const value1 = _.get(dataset1, key);
     const value2 = _.get(dataset2, key);
 
-    return '    ' + JSON.stringify({value1, value2});
+    // if (_.has(dataset1, key) && _.has(dataset2, key)
+    // && _.isPlainObject(value1) _.isPlainObject(value2)) {
+
+    // }
+    if (_.has(dataset1, key) && _.has(dataset2, key)
+      && _.isEqual(value1, value2)) {
+      return `    ${key}: ${value1}`;
+    }
+    if (_.has(dataset1, key) && _.has(dataset2, key)
+      && !_.isEqual(value1, value2)) {
+      return [`  - ${key}: ${value1}`, `  + ${key}: ${value2}`].join('\n');
+    }
+    if (_.has(dataset1, key) && _.has(dataset2, key)) {
+      return `    ${key}: ${value1}`;
+    }
+    if (_.has(dataset1, key) && !_.has(dataset2, key)) {
+      return `  - ${key}: ${value1}`;
+    }
+    if (!_.has(dataset1, key) && _.has(dataset2, key)) {
+      return `  + ${key}: ${value2}`;
+    }
   })
     .join('\n');
 
