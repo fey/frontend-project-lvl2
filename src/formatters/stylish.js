e282c796467e2504bd56ea5abbd61fe211917759
feycot <feycot@gmail.com>
2020-10-19 08:34:32 +0500

    wip

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,24 +1,39 @@
+import _ from 'lodash';
+
 const format = (diffTree) => {
+  const formatValue = (value) => {
+    // if (_.isPlainObject(value)) {
+    //   console.log(value);
+    //   return (value);
+    // }
+
+    if (_.isArray(value)) {
+      return `[ ${value.map(formatValue).join(', ')} ]`;
+    }
+
+    console.log(value);
+    return value.toString();
+  };
 
   const formatted = diffTree.map(({name, type, oldValue, newValue, children}) => {
     if (type === 'unchanged') {
-      return `    ${name}: ${oldValue}`;
+      return `    ${name}: ${formatValue(oldValue)}`;
     }
 
     if (type === 'changed') {
-      return `    ${name}: ${oldValue}`;
+      return `    ${name}: ${formatValue(oldValue)}`;
     }
 
     if (type === 'added') {
-      return `  + ${name}: ${newValue}`;
+      return `  + ${name}: ${formatValue(newValue)}`;
     }
 
     if (type === 'deleted') {
-      return `  - ${name}: ${oldValue}`;
+      return `  - ${name}: ${formatValue(oldValue)}`;
     }
 
     if (type === 'nested') {
-      return `    ${name}: nested`;
+      return `    ${name}: ${format(children)}`;
     }
 
     throw new Error('Unknown node type');
