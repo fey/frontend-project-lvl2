8931b776bca2ce523b1daf417e63f32f87358cdb
feycot <feycot@gmail.com>
2020-10-25 19:00:52 +0500

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,47 +8,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 describe('test gendiff', () => {
-  test.each([
-    ['yml'],
-    ['json'],
-    ['ini'],
-  ])('stylish with %s', (fileFormat) => {
+  const dataTable = [
+    ['stylish', 'yml'],
+    ['stylish', 'json'],
+    ['plain', 'yml'],
+    ['plain', 'json'],
+    ['json', 'yml'],
+    ['json', 'json'],
+  ];
+
+  test.each(dataTable)('%s format with %s files', (formatter, fileFormat) => {
     const filepath1 = path.resolve(__dirname, fixtureDir, `file1.${fileFormat}`);
     const filepath2 = path.resolve(__dirname, fixtureDir, `file2.${fileFormat}`);
-    const expectedPath = path.join(__dirname, fixtureDir, 'stylish.txt');
+    const expectedPath = path.join(__dirname, fixtureDir, `${formatter}.txt`);
 
-    const expected = fs.readFileSync(expectedPath, 'utf-8');
+    const expected = fs.readFileSync(expectedPath, 'utf-8').trim();
 
-    expect(gendiff(filepath1, filepath2)).toEqual(expected.trim());
-  });
-
-  test.each([
-    ['yml'],
-    ['json'],
-    ['ini'],
-  ])('plain with %s', (fileFormat) => {
-    const filepath1 = path.resolve(__dirname, fixtureDir, `file1.${fileFormat}`);
-    const filepath2 = path.resolve(__dirname, fixtureDir, `file2.${fileFormat}`);
-    const expectedPath = path.join(__dirname, fixtureDir, 'plain.txt');
-
-    const expected = fs.readFileSync(expectedPath, 'utf-8');
-    const actual = gendiff(filepath1, filepath2, 'plain');
-
-    expect(actual).toEqual(expected.trim());
-  });
-
-  test.each([
-    ['yml'],
-    ['json'],
-    ['ini'],
-  ])('json with %s', (fileFormat) => {
-    const filepath1 = path.resolve(__dirname, fixtureDir, `file1.${fileFormat}`);
-    const filepath2 = path.resolve(__dirname, fixtureDir, `file2.${fileFormat}`);
-    const expectedPath = path.join(__dirname, fixtureDir, 'json.txt');
-
-    const expected = fs.readFileSync(expectedPath, 'utf-8');
-    const actual = gendiff(filepath1, filepath2, 'json');
-
-    expect(actual).toEqual(expected.trim());
+    expect(gendiff(filepath1, filepath2, formatter)).toEqual(expected);
   });
 });
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,15 +2,17 @@
 import commander from 'commander';
 import gendiff from '../src/index.js';
 
-commander.program
+const { program } = commander;
+
+program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format');
 
-commander.program.parse();
+program.parse();
 
-const [filepath1, filepath2] = commander.program.args;
-const { format } = commander.program;
+const [filepath1, filepath2] = program.args;
+const { format } = program;
 
 console.log(gendiff(filepath1, filepath2, format));
