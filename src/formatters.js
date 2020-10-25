ac24077909cdc2d543cee6077798d0fe442ea103
feycot <feycot@gmail.com>
2020-10-25 15:35:08 +0500

    fix lint

--- src/formatters.js
+++ src/formatters.js
@@ -1,15 +1,10 @@
 import formatStylish from './formatters/stylish.js';
 
-/**
- *
- * @param {Object} diff
- * @param {string} type
- */
 const format = (diff, type = 'stylish') => {
   switch (type) {
     case 'stylish':
       return formatStylish(diff);
-    case 'plain': 
+    case 'plain':
       return null;
       // return formatPlain(diff);
     case 'json':
