30d38a6d81a58ed108558a2671bfed7046b19275
feycot <feycot@gmail.com>
2020-10-29 22:25:22 +0500

    refactor tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,15 +3,16 @@ import fs from 'fs';
 import path from 'path';
 import gendiff from '../src/gendiff.js';
 
-const fixtureDir = path.join('..', '__fixtures__');
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
 describe('test gendiff', () => {
   test('default formatter with json', () => {
-    const filepath1 = path.resolve(__dirname, fixtureDir, 'file1.json');
-    const filepath2 = path.resolve(__dirname, fixtureDir, 'file2.json');
-    const expectedPath = path.join(__dirname, fixtureDir, 'stylish.txt');
+    const filepath1 = getFixturePath('file1.json');
+    const filepath2 = getFixturePath('file2.json');
+    const expectedPath = getFixturePath('stylish.txt');
 
     const expected = fs.readFileSync(expectedPath, 'utf-8').trim();
 
@@ -28,9 +29,9 @@ describe('test gendiff', () => {
   ];
 
   test.each(dataTable)('%s format with %s files', (formatter, fileFormat) => {
-    const filepath1 = path.resolve(__dirname, fixtureDir, `file1.${fileFormat}`);
-    const filepath2 = path.resolve(__dirname, fixtureDir, `file2.${fileFormat}`);
-    const expectedPath = path.join(__dirname, fixtureDir, `${formatter}.txt`);
+    const filepath1 = getFixturePath(`file1.${fileFormat}`);
+    const filepath2 = getFixturePath(`file2.${fileFormat}`);
+    const expectedPath = getFixturePath(`${formatter}.txt`);
 
     const expected = fs.readFileSync(expectedPath, 'utf-8').trim();
 
