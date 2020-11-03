0ca7198a8cd495c25898a62f3bc6ced6ae873226
Nikolay Gagarinov <n.gagarinov@xsolla.com>
2020-11-03 19:10:06 +0500

    fix linter for tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -20,12 +20,12 @@ const formaterCases = [
 ];
 
 describe.each(formaterCases)('test with %s formatter, expected %s output', (formatter, expectedFileName) => {
-  // eslint-disable-line
+  // eslint-disable-next-line
   let expectedOutput;
 
   beforeEach(() => {
     const expectedPath = getFixturePath(`${expectedFileName}.txt`);
-    // eslint-disable-line
+    // eslint-disable-next-line
     expectedOutput = read(expectedPath);
   });
 
